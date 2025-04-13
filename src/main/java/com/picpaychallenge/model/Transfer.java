4dc3b5929f3606fb353141c3735d1be31ba01e6b
RodrigoPereir4 <digolima167@gmail.com>
2025-04-13 16:28:03 -0300

    [ADD] Model da classe Transfer

---
+++ src/main/java/com/picpaychallenge/model/Transfer.java
@@ -0,0 +1,41 @@
+package com.picpaychallenge.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_transfers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amout;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
